e8ce0125488341ddebbaa8315b88e70f44ab0434
TENET <tenet.it@ya.ru>
2023-06-09 23:04:44 +0300

    f commit

---
+++ task_n2.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+import requests
+
+TOKEN = ''
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href_json = self._get_upload_link(disk_file_path=disk_file_path)
+        href = href_json["href"]
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Успех")
+
+
+if __name__ == "__main__":
+    ya = YandexDisk(token=TOKEN)
+    ya.upload_file_to_disk('dik/test2.txt', 'test.txt')
